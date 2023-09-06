60955aadd388aee9e317d41acb743e293e1f7a68
Kevin Garcia <kevinede40@gmail.com>
2023-09-05 21:25:19 -0600

    ToDo Machine wth Interactions

---
+++ src/TodoCounter.js
@@ -0,0 +1,17 @@
+import "./TodoCounter.css";
+
+function TodoCounter({ total, completed }) {
+  return (
+    <>
+      {total === completed ? (
+        <h1 className="TodoCounter">Has completado todas tus tareas🫡🎉</h1>
+      ) : (
+        <h1 className="TodoCounter">
+          Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+        </h1>
+      )}
+    </>
+  );
+}
+
+export { TodoCounter };
