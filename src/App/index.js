9723c9cf30506024a844aa955952a79b70fcd1aa
Kevin Garcia <kevinede40@gmail.com>
2023-09-05 22:51:05 -0600

    Organización de archivos y carpetas

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButton';
+
+import {useLocalStorage} from './useLocalStorage'
+import {AppUI} from './AppUI'
 
 /* const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
@@ -15,30 +13,6 @@ import { CreateTodoButton } from './CreateTodoButton';
 
 //localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos))
 //localStorage.removeItem('TODOS_V1', defaultTodos)
-
-function useLocalStorage(itemName,initialValue){
-
-  const localStorageItem = localStorage.getItem(itemName)
-  let parsedItem
-  if(!localStorageItem){
-    localStorage.setItem(itemName, JSON.stringify(initialValue))
-    parsedItem = []
-  }
-  else{
-     parsedItem = JSON.parse(localStorageItem)
-
-  }
-
-  const [item, setItem]= React.useState(parsedItem)
-
-  const saveItem = (newItem) =>{
-    localStorage.setItem(itemName, JSON.stringify(newItem))
-    setItem(newItem)
-  }
-
-  return [item, saveItem]
-
-}
 
 function App() {
 
@@ -72,30 +46,18 @@ function App() {
     newTodos.splice(todoIndex, 1)
     saveTodos(newTodos)
   }
-  return (
-    <>
-      <TodoCounter completed={completedTodos} total={totalTodos} />
-      <TodoSearch searchvalue={searchValue} setSearchValue={setSearchValue}/>
 
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => {
-              completeTodo(todo.text)
-            }}
-            onDelete={() => {
-              deleteTodo(todo.text)
-            }}
-          />
-        ))}
-      </TodoList>
-      
-      <CreateTodoButton />
-    </>
-  );
+  return (
+    <AppUI
+    completedTodos={completedTodos}
+    totalTodos={totalTodos}
+    searchValue={searchValue}
+    setSearchValue={setSearchValue}
+    searchedTodos={searchedTodos}
+    completeTodo={completeTodo}
+    deleteTodo={deleteTodo}
+    />
+  )
 }
 
 export default App;
